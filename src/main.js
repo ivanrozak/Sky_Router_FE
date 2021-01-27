54d9f5288bbf08bdcc2d9991ef813c785860aae7
Cotllinz <63383858+Cotllinz@users.noreply.github.com>
2021-01-27 19:18:12 +0800

    Starter Pack

--- src/main.js
+++ src/main.js
@@ -1,12 +1,21 @@
-import Vue from "vue";
-import App from "./App.vue";
-import router from "./router";
-import store from "./store";
-
-Vue.config.productionTip = false;
+import Vue from 'vue'
+import App from './App.vue'
+import router from './router'
+import store from './store'
+import { BootstrapVue, IconsPlugin } from 'bootstrap-vue'
+import 'bootstrap/dist/css/bootstrap.css'
+import 'bootstrap-vue/dist/bootstrap-vue.css'
+import VueSweetalert2 from 'vue-sweetalert2'
+import 'animate.css'
+import 'sweetalert2/dist/sweetalert2.min.css'
+Vue.use(BootstrapVue)
+Vue.use(VueSweetalert2)
+Vue.use(BootstrapVue)
+Vue.use(IconsPlugin)
+Vue.config.productionTip = false
 
 new Vue({
   router,
   store,
   render: h => h(App)
-}).$mount("#app");
+}).$mount('#app')
---
+++ src/views/Auth/Login.vue
@@ -0,0 +1,3 @@
+<template>
+  <h1>Ini Login</h1>
+</template>
