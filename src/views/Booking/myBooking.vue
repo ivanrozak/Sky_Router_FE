59ec247c8cab8dddf31beab2f2d6a2a93ceaaee1
Risky Amalia Haris <amaliaharisr@gmail.com>
2021-01-28 12:52:42 +0700

    import Navbar Footer Profile to my Booking

--- src/views/Booking/myBooking.vue
+++ src/views/Booking/myBooking.vue
@@ -1,20 +1,30 @@
 <template>
-  <div class="mybooking-page">
-    <b-row>
-      <b-col cols="4"> </b-col>
-      <b-col cols="8">
-        <RightSideMyBooking />
-      </b-col>
-    </b-row>
+  <div>
+    <Navbar />
+    <div class="mybooking-page">
+      <b-row>
+        <b-col sm="12" md="4" lg="4"><LeftProfile /> </b-col>
+        <b-col sm="12" md="8" lg="8">
+          <RightSideMyBooking />
+        </b-col>
+      </b-row>
+    </div>
+    <Footer />
   </div>
 </template>
 
 <script>
 import RightSideMyBooking from '../../components/_base/My Booking Component/rightMyBookingComponent'
+import LeftProfile from '../../components/_base/Profile Component/LeftProfileComponent'
+import Navbar from '../../components/naviationBar'
+import Footer from '../../components/footer'
 export default {
   name: 'myBooking',
   components: {
-    RightSideMyBooking
+    RightSideMyBooking,
+    LeftProfile,
+    Navbar,
+    Footer
   }
 }
 </script>
