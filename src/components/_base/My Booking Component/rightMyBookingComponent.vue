82d886c7e2b1d9f7a92da3b09e7e49cb338a34cb
Risky Amalia Haris <amaliaharisr@gmail.com>
2021-01-28 09:13:07 +0700

    update show payment in my booking

--- src/components/_base/My Booking Component/rightMyBookingComponent.vue
+++ src/components/_base/My Booking Component/rightMyBookingComponent.vue
@@ -40,43 +40,65 @@
       <div v-show="isClickDetails">
         <div class="passenger-detail">
           <div class="box-detail">
-            <h5>Passenger Details</h5>
-            <div class="line"></div>
-            <b-row>
+            <b-row align-v="baseline">
               <b-col md="auto">
-                <h6>Airline</h6>
                 <img
-                  style="width:60px"
+                  style="width:100px"
                   src="../../../assets/myBooking/GA_900.png"
                   alt=""
                 />
               </b-col>
               <b-col>
-                <h6>Route</h6>
-                <p style="font-size: 20px;font-weight: 600">
-                  IDN
-                  <span
+                <p class="airline-name">Garuda Indonesia</p>
+              </b-col>
+            </b-row>
+            <br /><br />
+            <p style="font-size: 18px;font-weight: 600; line-height: 27px">
+              Medan (IDN)
+              <span
+                ><img src="../../../assets/myBooking/airplaneIcon.png" alt=""
+              /></span>
+              Tokyo (JPN)
+            </p>
+            <b-row>
+              <b-col
+                ><p class="grey">
+                  Sunday, 15 August 2020<span
                     ><img
-                      src="../../../assets/myBooking/airplaneIcon.png"
-                      alt=""
-                  /></span>
-                  JPN
-                </p>
-              </b-col>
-              <b-col>
-                <h6>Departure Date</h6>
-                <p>20 July 2020</p>
-              </b-col>
-              <b-col md="auto">
-                <h6>Airtime</h6>
-                <p>10.00 - 20.00</p>
-              </b-col>
-              <b-col md="auto"
-                ><h6>Total Payment</h6>
-                <p>IDR 50.200.000,00</p></b-col
+                      src="../../../assets/myBooking/Ellipse 48.png"
+                      alt=""/></span
+                  >12:33 - 15:21
+                </p></b-col
               >
             </b-row>
-            <b-table hover :items="passengers"></b-table>
+            <b-row>
+              <b-col md="auto"
+                ><div class="frame">
+                  <img
+                    style="position: absolute; top:8px; left: 23px"
+                    src="../../../assets/myBooking/Vector.png"
+                    alt=""
+                  /></div
+              ></b-col>
+              <b-col class="blue">Refundable</b-col>
+            </b-row>
+            <br />
+            <b-row>
+              <b-col md="auto"
+                ><div class="frame">
+                  <img
+                    style="position: absolute; top:8px; left: 23px"
+                    src="../../../assets/myBooking/Vector.png"
+                    alt=""
+                  /></div
+              ></b-col>
+              <b-col class="blue">Can Reschedule</b-col>
+            </b-row>
+            <br />
+            <b-row>
+              <b-col><h5>Total Payment</h5></b-col>
+              <b-col class="right">@145.00</b-col>
+            </b-row>
           </div>
         </div>
       </div>
@@ -138,8 +160,23 @@ export default {
 </script>
 
 <style scoped>
-h6 {
+.blue {
   color: #2395ff;
+}
+.frame {
+  border-style: solid;
+  border-color: #2395ff;
+  border-radius: 50%;
+  width: 25px;
+  height: 25px;
+  text-align: center;
+  border-width: 1px;
+}
+.airline-name {
+  color: #595959;
+  font-weight: 500;
+  font-size: 16px;
+  line-height: 22px;
 }
 .title {
   letter-spacing: 0.3em;
@@ -208,5 +245,10 @@ span {
 .passenger-detail,
 .box-detail {
   padding: 20px;
+}
+.box-detail {
+  width: 350px;
+  box-shadow: 3px 3px 5px 6px #979797;
+  border-radius: 15px;
 }
 </style>
