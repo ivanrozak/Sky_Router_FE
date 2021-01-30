f24d0ca2d2dd6770edc8fc9c04cc09d0f23179f0
Cotllinz <63383858+Cotllinz@users.noreply.github.com>
2021-01-30 14:03:06 +0800

    Notif Update

---
+++ src/components/_base/NavbarShowMenu/Notification.vue
@@ -0,0 +1,91 @@
+<template>
+  <b-card class="notification">
+    <h5>NOTIFICATIONS</h5>
+    <div class="title_notif d-flex align-items-center">
+      <h2>
+        Notifications
+      </h2>
+      <a class="ml-auto">Clear</a>
+    </div>
+    <div class="scroll mt-lg-3 pr-1">
+      <b-card
+        v-for="(items, index) in data"
+        :key="index"
+        class="notif_message mb-3"
+      >
+        <h5>Congratulation</h5>
+        <p>
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+          eiusmod tempor incididunt ut labore....
+        </p>
+        <p class="mt-lg-3">5h ago</p>
+      </b-card>
+    </div>
+  </b-card>
+</template>
+<script>
+export default {
+  name: 'Notif',
+  data() {
+    return {
+      data: 7
+    }
+  }
+}
+</script>
+<style scoped>
+.notification h5 {
+  font-size: 14px;
+  letter-spacing: 5px;
+  color: #2395ff;
+}
+.title_notif h2 {
+  font-weight: 600;
+  font-size: 24px;
+}
+.title_notif a {
+  cursor: pointer;
+  text-decoration: none;
+  font-weight: 600;
+  font-size: 16px;
+}
+.scroll {
+  overflow-y: scroll;
+  overflow-x: hidden;
+  height: 550px;
+}
+.scroll::-webkit-scrollbar {
+  width: 5px;
+}
+.scroll::-webkit-scrollbar-track {
+  box-shadow: inset 0 0 6px rgba(0, 0, 0, 0.3);
+  border-radius: 10px;
+}
+.scroll::-webkit-scrollbar-thumb {
+  border-radius: 10px;
+  box-shadow: inset 0 0 6px rgba(0, 0, 0, 0.5);
+}
+.notif_message {
+  border: 2px solid #3d9bf3;
+  border-radius: 10px;
+  background: #f7fbff;
+}
+.notif_message p {
+  margin-bottom: 0;
+  font-size: 14px;
+}
+.notif_message h5 {
+  font-weight: 700;
+  font-size: 16px;
+  letter-spacing: 0;
+}
+.notification {
+  position: absolute;
+  border-radius: 20px;
+  font-family: 'Poppins', sans-serif;
+  z-index: 2;
+  width: 500px;
+  right: -200px;
+  top: 70px;
+}
+</style>
---
+++ src/components/_base/NavbarShowMenu/NotificationMessage.vue
@@ -0,0 +1,51 @@
+<template>
+  <b-card class="position-absolute notification">
+    <h5>Chat</h5>
+    <div class="title_notif d-flex align-items-center">
+      <h2>
+        Chat
+      </h2>
+      <a>Filter</a>
+    </div>
+    <div
+      v-for="(items, index) in data"
+      :key="index"
+      class="scroll  mt-lg-3 pr-1"
+    >
+      <img class="image_friendProfile" alt="image_chatfriend" />
+      <div class="ml-lg-3 name_tag mt-lg-3">
+        <h2>Soham Henry</h2>
+        <p class="mt-lg-2">Bro, just fuck off</p>
+      </div>
+      <div class="desc_time ml-auto mr-lg-2">
+        <h3>17.00</h3>
+        <span  class="badge">10</span>
+      </div>
+    </div>
+  </b-card>
+</template>
+<script>
+import moment from 'moment'
+export default {
+  name: 'Notif',
+  data() {
+    return {
+      data: 7
+    }
+  },
+  methods: {
+    formatTime(value) {
+      moment.locale('ID')
+      return moment(String(value)).format('LT')
+    }
+  }
+}
+</script>
+<style scoped>
+.notification {
+  z-index: 2;
+  width: 400px;
+  right: -150px;
+  top: 70px;
+}
+</style>
