bee5de206dbcfb10fd418dfeb3634b2588dfb453
Anka-Abdullah <ankaabdullah.id@gmail.com>
2021-02-01 19:04:47 +0700

    anka

---
+++ src/views/PostSchedule.vue
@@ -0,0 +1,13 @@
+<template>
+  <div>
+    <b-container>
+      <h1>Post Schedule</h1>
+      <label for=""></label>
+    </b-container>
+    <input type="text" />
+  </div>
+</template>
+<script>
+export default {}
+</script>
+<style scooped></style>
