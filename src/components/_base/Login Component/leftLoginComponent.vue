b947cbe52c764b62e164a1a8dcc4146be50c8e35
Cotllinz <63383858+Cotllinz@users.noreply.github.com>
2021-02-05 04:27:22 +0800

    Update Responsif Login Page

--- src/components/_base/Login Component/leftLoginComponent.vue
+++ src/components/_base/Login Component/leftLoginComponent.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="bg_login d-flex">
     <img
-      class="image_login"
+      class="image_login d-none d-lg-block"
       src="../../../assets/Images/Main Image/image_login.svg"
       alt="imageBackground"
     />
@@ -20,5 +20,12 @@ export default {
 .image_login {
   margin: 0 auto;
   display: block;
+}
+
+@media (max-width: 576px) {
+  .bg_login {
+    background: #ffffff;
+    height: 0;
+  }
 }
 </style>
--- src/components/_base/Login Component/rightLoginComponent.vue
+++ src/components/_base/Login Component/rightLoginComponent.vue
@@ -2,18 +2,15 @@
   <div class="login_form">
     <div class="logo_login d-flex align-items-center pt-lg-5 ">
       <img
+        class="d-none d-lg-block"
         src="../../../assets/Images/Logo/illustration.svg"
         alt="image_logo"
       />
-      <span class="ml-2">SkyRouter</span>
+      <span class="ml-lg-2 pt-4 pt-lg-0">SkyRouter</span>
     </div>
     <b-form @submit.prevent="onSubmit" class="form_login ">
       <h1>Login</h1>
-      <b-form-group
-        class="pt-lg-3"
-        id="input-group-1"
-        description="We'll never share your email with anyone else."
-      >
+      <b-form-group class="pt-lg-3 pt-3" id="input-group-1">
         <b-form-input
           id="input-1"
           type="email"
@@ -21,6 +18,9 @@
           placeholder="Email"
           required
         ></b-form-input>
+        <label class="labels" for=""
+          >We'll never share your email with anyone else.</label
+        >
       </b-form-group>
       <b-form-group id="input-group-2">
         <b-form-input
@@ -31,11 +31,11 @@
           required
         ></b-form-input>
       </b-form-group>
-      <button type="submit" class="btn_signin w-100 mt-lg-4 py-lg-3">
+      <button type="submit" class="btn_signin w-100 py-3 mt-3 mt-lg-4 py-lg-3">
         Sign in
       </button>
       <div class="reset_btn text-center">
-        <p style="margin-bottom: 0;" class=" mt-lg-3">
+        <p style="margin-bottom: 0;" class=" mt-lg-3 mt-3">
           Did you forgot your password?
         </p>
         <a @click="onReset" class="btn_reset">Tap here for reset</a>
@@ -45,13 +45,16 @@
     <b-col class="btn_loginbyType" lg="12">
       <p class="text-center">or sign in with</p>
       <div class="d-flex justify-content-center">
-        <button type="button" class="btn_google px-lg-4 py-lg-2">
+        <button type="button" class="btn_google py-2 px-4 px-lg-4 py-lg-2">
           <img
             src="../../../assets/Images/Logo/google.svg"
             alt="Icon_btnGoogle"
           />
         </button>
-        <button type="button" class="btn_facebook px-lg-4 py-lg-2 ml-3">
+        <button
+          type="button"
+          class="btn_facebook py-2 px-4 px-lg-4 py-lg-2 ml-3"
+        >
           <img
             src="../../../assets/Images/Logo/FB.svg"
             alt="Icon_btnFacebook"
@@ -152,6 +155,9 @@ export default {
   box-shadow: 0px 8px 10px rgba(35, 149, 255, 0.3);
   border-radius: 10px;
 }
+.labels {
+  color: #88898a;
+}
 .btn_signin:hover {
   background: #118cff;
 }
@@ -160,5 +166,58 @@ input {
   height: 50px;
   border: none;
   border-bottom: 2px solid rgba(210, 194, 255, 0.68);
+}
+@media (max-width: 1430px) {
+  .form_login {
+    padding: 7% 25% 0 25%;
+  }
+}
+@media (max-width: 1280px) {
+  .form_login {
+    padding: 15% 15% 0 15%;
+  }
+  .login_form {
+    height: 100vh;
+    max-height: 100vh;
+  }
+}
+@media (max-width: 576px) {
+  .form_login {
+    padding: 40% 0 0 0;
+  }
+  .btn_signin,
+  .btn_signin:hover {
+    background: #ffffff;
+    color: #118cff;
+  }
+  .labels {
+    color: #ffffff;
+  }
+  .btn_google,
+  .btn_facebook,
+  .btn_google:hover,
+  .btn_facebook:hover {
+    background: #ffffff;
+    border: 1px solid #ffffff;
+  }
+  .reset_btn a,
+  .reset_btn a:hover {
+    color: #ffffff;
+  }
+  .logo_login span {
+    font-family: 'Poppins', sans-serif;
+    font-weight: 700;
+    font-size: 33px;
+    color: #ffffff;
+  }
+  .logo_login {
+    padding-left: 0%;
+  }
+}
+@media (max-width: 375px) {
+  .form_login {
+    padding: 40% 0 0 0;
+  }
+ 
 }
 </style>
